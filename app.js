429260d23018d31629f68e067a03d2d0173318a5
Daniel02-03-04 <danielramos550455@gmail.com>
2023-08-10 15:44:07 -0300

    productCart

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 //rutas
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
-
+app.get("/productCart", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/productCart.html"));
+});
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
 
 app.listen(3000, () => {
   console.log("Success");
