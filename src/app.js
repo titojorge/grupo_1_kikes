fa12d29b0c69682c65cb669ee46f5013536df402
Leonardo Rafael Guanuco <leomusicocompositor@gmail.com>
2023-09-19 15:04:25 -0300

    Tocando el css del login y del home

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const methodOverride =  require('method-override'); // Pasar poder usar los mét
 //config
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // Formularios sean procesados
 app.use(express.json());
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 //config ejs
