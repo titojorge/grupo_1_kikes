634c817fa1bcb7b0150604817a8c641f3436d0bd
GonzaloG18 <fragmentado1811@gmail.com>
2023-10-04 23:17:57 -0300

    Agregue el multer de subir imagenes en main,maincontroller y app.js

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,22 +1,49 @@
 const fs = require('fs');
 const path = require('path');
-
+const multer = require('multer');
+const usersFilePath = path.join(__dirname, '../data/user.json');
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images/perfiles');
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+    }
+});
+const upload = multer({ storage: storage }).single('imagen_perfil');
 
 const mainController = {
     home: (req, res) => {
-        return res.render('home', { products : products});
-        //res.sendFile(path.join(__dirname, '../views/home.html'));
+        return res.render('home', { products: products });
     },
     login: (req, res) => {
         return res.render('./users/login');
-        //res.sendFile(path.resolve(__dirname, '../views/login.html'));
     },
     register: (req, res) => {
-        return res.render('./users/register');
-        //res.sendFile(path.resolve(__dirname, '../views/register.html'));
+        upload(req, res, (err) => {
+            if (err) {
+                return res.send('Error al subir el archivo.');
+            }
+            const { nombre, apellido, email, contrasenia } = req.body;
+            const imagen = req.file ? '/images/perfiles/' + req.file.filename : '';
+            const newUser = {
+                Identificador: users.length + 1,
+                Nombre: nombre,
+                Apellido: apellido,
+                Email: email,
+                Contrasenia: contrasenia,
+                Categoria: "Customer",
+                Imagen: imagen
+            };
+            users.push(newUser);
+            fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2), 'utf-8');
+            return res.redirect('/');
+        });
     },
 }
-
 module.exports = mainController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 const productsController = require('../controllers/productsController');
+const multer = require('multer');
+
+// Configuración de Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images/perfiles');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  }
+});
+const upload = multer({ storage: storage });
 
 router.get('/', mainController.home)
-
 router.get('/home', mainController.home)
-
 router.get('/login', mainController.login)
-router.get('/register', mainController.register)
+router.get('/register', mainController.register);
 router.get('/detail',productsController.detail)
 router.get('/productCart',productsController.productCart)
 
