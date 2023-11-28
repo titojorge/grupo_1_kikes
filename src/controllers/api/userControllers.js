88b885175a0566e90f24a36adf46ef46e20bf300
Daniel Ramos <danielramos550455@gmail.com>
2023-11-28 20:38:45 -0300

    api completa de usuarios

--- src/controllers/api/userControllers.js
+++ src/controllers/api/userControllers.js
@@ -7,9 +7,23 @@ const userApiControllers = {
         db.Usuario
             .findAll()
             .then( usuarios => {
+
+                let data = [];
+                
+                usuarios.forEach(user => {
+                    let dataUser = {
+                        id: user.id,
+                        nombre: user.nombre,
+                        apellido: user.apellido,
+                        email: user.email,
+                        detail: "localhost:3000/api/users/" + user.id
+                    };
+                    data.push(dataUser);
+                });
                 return res.json({
                     total: usuarios.length,
-                    data: usuarios
+                    data: data,
+
                 })
             } )
     },
@@ -18,7 +32,18 @@ const userApiControllers = {
             .findByPk(req.params.id)
             .then( usuario => {
                 return res.json({
-                    data: usuario
+                    data: {
+                        id: usuario.id,
+                        nombre: usuario.nombre,
+                        apellido: usuario.apellido,
+                        email: usuario.email,
+                        sexo: usuario.sexo,
+                        imagen: "localhost:3000" + usuario.imagen,
+                        fecha_nacimiento: usuario.fecha_nacimiento,
+                        fecha_creacion: usuario.fecha_creacion,
+                        fecha_modificacion: usuario.fecha_modificacion,
+                        fecha_borrado: usuario.fecha_borrado
+                    }
                 })
             } )
     }
