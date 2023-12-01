e60ec4687a74232c2ed006597453034ddc66743a
jorgezoto <tito.jorge12@gmail.com>
2023-12-01 19:15:41 -0300

    Arreglo del login

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -11,6 +11,8 @@ const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 const moment = require("moment");
 const { log } = require('console');
+const validateLogin = require('../middlewares/validateLogin');
+const { validationResult } = require('express-validator')
 
 const mainController = {
     home: (req, res) => {
@@ -31,25 +33,31 @@ const mainController = {
         })
         Promise.all([promesaProductos, promesaUsuario])
         .then( function ([products, user_found]) {
-            if (user_found && user_found.length > 0) {
-                const check = bcrypt.compareSync(pass_form, user_found[0].contrasenia);
-                if (check) {
-                    req.session.id_usuario = user_found[0].id;
-                    req.session.nombre = user_found[0].nombre;
-                    req.session.apellido = user_found[0].apellido;
-                    res.cookie('id', user_found[0].id);
-                    res.cookie('nameUser', user_found[0].nombre);
-                    res.render('home', { products: products, userFound: user_found })
-                    console.log('exito');
+            let resultValidation = validationResult(req)
+		    if(resultValidation.errors.length > 0){
+                let errores = resultValidation.mapped()
+                console.log(errores);
+                return res.render('./users/login', { errors: errores, oldData: req.body })
+            }else{
+                if (user_found && user_found.length > 0) {
+                    const check = bcrypt.compareSync(pass_form, user_found[0].contrasenia);
+                    if (check) {
+                        req.session.id_usuario = user_found[0].id;
+                        req.session.nombre = user_found[0].nombre;
+                        req.session.apellido = user_found[0].apellido;
+                        res.cookie('id', user_found[0].id);
+                        res.cookie('nameUser', user_found[0].nombre);
+                        res.render('home', { products: products, userFound: user_found })
+                        console.log('exito');
+                    } else {
+                        res.render('./users/login',{errors: {contrasenia:{msg:'Contraseña Incorrecta123'}}})
+                        console.log('fracaso');
+                    }
                 } else {
-                    res.render('./users/login')
-                    console.log('fracaso');
+                    res.render('./users/login',{errors: {email:{msg:'Usuario inexistente'}}});
+                    console.log('Usuario no encontrado');
                 }
-            } else {
-                res.render('./users/login');
-                console.log('Usuario no encontrado');
-            }
-		  })
+		  }})
         
     },
     cerrar: (req, res) => {
