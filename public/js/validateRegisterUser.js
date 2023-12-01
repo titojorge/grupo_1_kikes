124cc20aa9707a43276f40625a167bc6ab1db9fc
Daniel Ramos <danielramos550455@gmail.com>
2023-12-01 17:18:30 -0300

    casi terminado la validación del front del register

--- public/js/validateRegisterUser.js
+++ public/js/validateRegisterUser.js
@@ -8,13 +8,8 @@ window.onload = function () {
         let errors = [];
         let inputName = document.getElementById('nombre');
         let inputSurname = document.getElementById('apellido');
-        let inputEmail = document.getElementById('email');
-        let inputConfirmarEmail = document.getElementById('confirmar_email');
         let inputPassword = document.getElementById('contrasenia');
         let inputConfirmarPassword = document.getElementById('confirmar_contrasenia');
-        let inputCategoria = document.getElementById('categoria');
-        //let inputFechaNacimiento = document.getElementById('fecha_nacimiento');
-        let inputSexo = document.getElementById('sexo');
         let inputImagen = document.getElementById('imagen_perfil');
         
         if(inputName.value == ''){
@@ -22,7 +17,7 @@ window.onload = function () {
                     field: inputName.name,
                     msg: "Campo Vacio!"
                 })
-            } else{
+            } else {
                 if(inputName.value.length < 2){
                     errors.push({
                         field: inputName.name,
@@ -45,31 +40,18 @@ window.onload = function () {
                 }
             }
 
-        if (inputEmail.value == '') {
-            errors.push({
-                field: inputEmail.name,
-                msg: "Campo Vacio!"
-            })
-        } else {
-
-        }
-
-        if (inputConfirmarEmail.value == '') {
-            errors.push({
-                field: inputConfirmarEmail.name,
-                msg: "Campo Vacio!"
-            })
-        } else {
-            
-        }
-
         if (inputPassword.value == '') {
             errors.push({
                 field: inputPassword.name,
                 msg: "Campo Vacio!"
             })
         } else {
-            
+            if (inputSurname.value.length < 8) {
+                errors.push({
+                    field: inputSurname.name,
+                    msg: "El campo debe tener al menos 8 caracteres"
+                })
+            }
         }
 
         if (inputConfirmarPassword.value == '') {
@@ -78,7 +60,12 @@ window.onload = function () {
                 msg: "Campo Vacio!"
             })
         } else {
-            
+            if (inputSurname.value.length != inputPassword.value) {
+                errors.push({
+                    field: inputSurname.name,
+                    msg: "Contraseña incorrecta"
+                })
+            }
         }
 
         let tipo = inputImagen.files[0].type
@@ -87,6 +74,16 @@ window.onload = function () {
                 field: inputImagen.name,
                 msg: "Solo se permiten extensiones de tipo .png, .jpg, .gif"
             })
+        }
+
+        if (errors.length > 0) {
+            document.querySelector('ul.text-danger').innerHTML = ''
+            console.log(errors);
+            errors.forEach((error) => {
+                ulError.innerHTML += "<li>" + error.field + ": " + error.msg + "</li>";
+            });
+        } else {
+            form.submit();
         }
 
     })
