c366bb35a7d03d316a81d4530da46fa073ed8601
Leonardo Rafael Guanuco <leomusicocompositor@gmail.com>
2023-12-01 20:10:56 -0300

    Se realizo -busqueda de productos-

--- src/routes/products.js
+++ src/routes/products.js
@@ -14,6 +14,9 @@ let validateEditProduct = require("../middlewares/validateEditProduct.js")
 router.get('/list', productsController.index);
 router.get('/productCart',guestMiddleware ,productsController.productCart) // Ruta que se accede con login
 router.get('/detail/:id', productsController.detail); 
+//router.get('/searchResult', productsController.search);
+router.post('/searchResult', productsController.search);
+
 /*New product*/
 router.get('/new', guestMiddleware ,productsController.new)  // Ruta que se accede con login, de aqui al final todas con login
 router.post('/new', guestMiddleware, upload.single('image_product'), validateCreateProduct ,productsController.store);
@@ -22,8 +25,7 @@ router.get('/edit/:id', guestMiddleware ,productsController.edit)
 router.put('/edit/:id', guestMiddleware ,upload.single('image-product'),validateEditProduct,productsController.update);   
 /*** DELETE ONE PRODUCT***/ 
 router.get('/delete/:id' ,productsController.delete); 
-router.delete('/delete/:id',productsController.destroy); 
-
+router.delete('/delete/:id',productsController.destroy);
 router.get('/crud/' ,productsController.crud); 
 
 
