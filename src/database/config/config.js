d8a9d998927b014781baa03aa40a72b169a0ad05
Daniel Ramos <danielramos550455@gmail.com>
2023-12-05 21:57:29 -0300

    css list users

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "123456",
+    "password": "",
     "database": "kike_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
