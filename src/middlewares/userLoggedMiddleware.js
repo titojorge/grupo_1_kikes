64b19be6d44eda1a62a8032b3f22e68ca9d71182
jorgezoto <tito.jorge12@gmail.com>
2023-12-07 22:28:19 -0300

    leo me corrijio un estilo

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -16,12 +16,10 @@ function userLoggedMiddleware(req, res, next) {
       }
     });
   }
-  console.log('Hola');
   console.log(req.session.userLogged);
   if (req.session.userLogged) {
     res.locals.isLogged = true;
     res.locals.userLogged = req.session.userLogged;
-    console.log(res.locals.userLogged);
   }
 
   next();
