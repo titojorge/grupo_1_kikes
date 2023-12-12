8b09b33669f58200d959a3e953459fb38ca27cfd
jorgezoto <tito.jorge12@gmail.com>
2023-12-12 18:05:24 -0300

    algunas funcionalidades menores

--- public/js/header.js
+++ public/js/header.js
@@ -7,5 +7,14 @@ window.addEventListener('load', ()=> {
             btnDropdownMenu.style.cssText = (btnDropdownMenu.style.display == 'none') ? 'display:block':'display:none'
         })   
     }
+
+    let btn_suscribirse = document.getElementById("suscribirse")
+    
+    
+    btn_suscribirse.addEventListener("click", function() {
+        // Aquí puedes agregar el código para manejar la acción de compra
+        document.getElementById("email_suscripcion").value = "";
+        alert("Felicidades ud se ha suscripto exitosamente a la pagina.");
+      });
     
 })
